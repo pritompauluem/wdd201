bbf170ea6a4b166c791b6eb637b2ebc148fbfb55
pritompauluem <112893655+pritompauluem@users.noreply.github.com>
2024-09-23 23:49:22 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const minimist = require("minimist");
 
-// Use minimist to parse command line arguments for port
+// Parse command-line arguments for the port
 const args = minimist(process.argv.slice(2));
 const port = args.port || 3000; // Default port is 3000 if not specified
 
